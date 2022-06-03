8be07bebb74449b532b28a4244ce061a1ff8a5d9
okyalos76 <93878437+okyalos76@users.noreply.github.com>
2022-06-03 20:08:28 +0200

    Update EQ-Comm-View.js

--- EQ-Comm-View.js
+++ EQ-Comm-View.js
@@ -1,1 +1,2 @@
- jq('.qx-pt-section').parent().parent().parent().parent().after(jq('#mainPage'));
+console.log('setting default MF-Call View values');
+jq('.qx-pt-section').parent().parent().parent().parent().after(jq('#mainPage'));
