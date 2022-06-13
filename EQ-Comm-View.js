841b39f63aa79840d8c934f2e8a0a4886adca9bc
okyalos76 <93878437+okyalos76@users.noreply.github.com>
2022-06-13 14:45:13 +0200

    Update EQ-Comm-View.js

--- EQ-Comm-View.js
+++ EQ-Comm-View.js
@@ -1,2 +1,2 @@
 console.log('setting default EQ-Comm View values');
-jq('.qx-pt-section').parent().parent().parent().parent().after(jq('#mainPage'));
+jq('.qx-pt-section').parent().parent().parent().parent().after(jq('#mainPage')).hide();
