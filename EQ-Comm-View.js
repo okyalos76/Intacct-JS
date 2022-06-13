4a241d3b21e6d4e51cd136a0eb93ac84e47d6d36
okyalos76 <93878437+okyalos76@users.noreply.github.com>
2022-06-13 14:47:50 +0200

    Update EQ-Comm-View.js

--- EQ-Comm-View.js
+++ EQ-Comm-View.js
@@ -1,2 +1,3 @@
 console.log('setting default EQ-Comm View values');
-jq('.qx-pt-section').parent().parent().parent().parent().after(jq('#mainPage')).hide();
+jq('.qx-pt-section').parent().parent().parent().parent().after(jq('#mainPage'));
+  jq('#editbuttid').prop("disabled",true);
